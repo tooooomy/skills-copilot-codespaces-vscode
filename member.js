00e3859afcc133c103ac7bd34322f18361d87660
tooooomy <166357619+tooooomy@users.noreply.github.com>
2024-11-14 07:57:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    var skills = [
+        'JavaScript',
+        'React',
+        'Node.js',
+        'MongoDB',
+        'Express',
+        'HTML',
+        'CSS',
+        'Python',
+        'Django',
+        'Flask',
+        'PostgreSQL',
+        'MySQL'
+    ];
+    return skills;
+}
